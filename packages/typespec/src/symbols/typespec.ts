39b46747944806dd8bf113d2bef6839cf5f55ca1
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2025-12-24 06:51:21 +0000

    more

---
+++ packages/typespec/src/symbols/typespec.ts
@@ -0,0 +1,66 @@
+import {
+  Namekey,
+  OutputDeclarationSpace,
+  OutputMemberSpace,
+  OutputSpace,
+  OutputSymbol,
+  OutputSymbolOptions,
+} from "@alloy-js/core";
+import { TypeSpecScope } from "../scopes/typespec.js";
+import { NamespaceSymbol } from "./namespace.js";
+
+// Need to think over the options here
+export interface TypeSpecSymbolOptions extends OutputSymbolOptions {}
+
+export type TypeSpecSymbolKinds = "symbol" | "namespace" | "named-type";
+
+export class TypeSpecSymbol extends OutputSymbol {
+  public readonly symbolKind: TypeSpecSymbolKinds = "symbol";
+
+  constructor(
+    name: string | Namekey,
+    spaces: OutputSpace[] | OutputSpace | undefined,
+    options: TypeSpecSymbolOptions = {},
+  ) {
+    super(name, spaces, options);
+  }
+
+  copy(): OutputSymbol {
+    const options = this.getCopyOptions();
+    const copy = new TypeSpecSymbol(super.name, undefined, {
+      ...options,
+    });
+    this.initializeCopy(copy);
+    return copy;
+  }
+
+  get enclosingNamespace(): NamespaceSymbol | undefined {
+    if (this.spaces.length === 0) {
+      return undefined;
+    }
+
+    // todo: probably need to validate that a symbol can't belong to spaces in
+    // multiple namespaces.
+    const firstSpace = this.spaces[0];
+
+    if (firstSpace instanceof OutputMemberSpace) {
+      // this symbol is a member of something, so get the enclosing namespace from
+      // the symbol.
+
+      if (firstSpace.symbol.constructor.name === "NamespaceSymbol") {
+        // this is a namespace symbol, so return the namespace symbol itself.
+        // can't use instanceof here due to circular reference issues.
+        return firstSpace.symbol as NamespaceSymbol;
+      }
+
+      return (firstSpace.symbol as TypeSpecSymbol).enclosingNamespace;
+    } else if (firstSpace instanceof OutputDeclarationSpace) {
+      // this symbol is in a lexical scope, so get the namespace symbol from the
+      // scope.
+      return (firstSpace.scope as TypeSpecScope).enclosingNamespace;
+    }
+    throw new Error("No place to get namespace symbol from");
+
+    // return undefined;
+  }
+}
