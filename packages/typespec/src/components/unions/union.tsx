dd4e0acba87a4a5614ea9c536cd0fe699d3290f8
Jose Alvarez <jp.alvarezl@gmail.com>
2026-02-09 11:16:14 +0100

    Probably don't need the namespace stuff

--- packages/typespec/src/components/unions/union.tsx
+++ packages/typespec/src/components/unions/union.tsx
@@ -5,13 +5,9 @@ import { NamespaceSymbol } from "../../symbols/namespace.js";
 export interface UnionProps {
   variants: Map<string, string>; // empty unions are not supported
   name: string;
-  namespace?: NamespaceSymbol;
 }
 
 export function Union(props: UnionProps) {
-  const namespaceCtx = useNamespaceContext();
-  const namespace = props.namespace ?? namespaceCtx?.symbol;
-
   // TODO figure out the right way to assert this
   // assert(props.variants.size > 0, "Unions must have at least one variant");
   return (
