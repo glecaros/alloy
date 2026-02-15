7835b8151d49a20288d9a8d2906b7953cda92e49
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2026-02-15 23:34:21 +0000

    Adding scalar

---
+++ packages/typespec/src/components/scalar-declaration/scalar-declaration.tsx
@@ -0,0 +1,32 @@
+import { Child, Declaration, Name, Namekey, Refkey } from "@alloy-js/core";
+import { useTypeSpecNamePolicy } from "../../name-policy.js";
+import { createNamedTypeSymbol } from "../../symbols/factories.js";
+import { ValueOrArray } from "../../util.js";
+
+export interface ScalarDeclarationProps {
+  name: string | Namekey;
+  refkey?: ValueOrArray<Refkey>;
+  is?: Child;
+  extends?: Child;
+}
+
+export function ScalarDeclaration(props: ScalarDeclarationProps) {
+  const sym = createNamedTypeSymbol(props.name, "scalar", {
+    refkeys: props.refkey,
+    namePolicy: useTypeSpecNamePolicy().for("scalar"),
+  });
+  if (props.is && props.extends) {
+    throw new Error(
+      "A scalar declaration cannot have both 'is' and 'extends' properties.",
+    );
+  }
+  return (
+    <>
+      <Declaration symbol={sym}>
+        scalar <Name />
+        {props.is && <> is {props.is}</>}
+        {props.extends && <> extends {props.extends}</>}
+      </Declaration>
+    </>
+  );
+}
